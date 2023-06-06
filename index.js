db3f25bfa623736ab7312dd1cb49706e49295212
git config --global user.name persicodegit config --global user.name <peachycodet@mail.ru>
2023-06-06 21:31:26 +0300

    Create entry and output points in webpack.config.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const post = new Post ('Webpack Post Title')
-
-console.log('Post to String:', post.toString())
-
-// Главный js файл
-// Активирует скрипт post.js, который и выводит в консоль информацию о дате и заголовок
