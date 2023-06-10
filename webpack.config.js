475175a5cbcb3cafa25bde9517bd08ad3a0c4f26
git config --global user.name persicodegit config --global user.name <peachycodet@mail.ru>
2023-06-10 07:30:02 +0300

    Add css minimization plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,28 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const CSSMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new TerserWebpackPlugin(),
+            new CSSMinimizerWebpackPlugin()
+        ]
+    }
+
+    return config
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -22,11 +41,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
 
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
 
     devServer: {
         port: 4200,
@@ -61,7 +76,8 @@ module.exports = {
         ),
         new MiniCSSExtractPlugin({
             filename: '[name].[contenthash].css'
-        })
+        }),
+        new CSSMinimizerWebpackPlugin()
     ],
 
     module: {
